7bf13b84e8e11c51d58aa851d6ae58e3fbe3f000
fariba <fmassah@cj.com>
2019-02-23 16:34:48 -0800

    Add routes

--- api/server.js
+++ api/server.js
@@ -1,15 +1,20 @@
-const express = require('express'); //TODO make es6 notation work
+const express = require('express'); //NodeJS not currently supporting ES6 imports & exports
 const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
-const port = process.env.API_PORT || 3000;
+const port = process.env.API_PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
 
-router.get('/', (req, res) => res.json({message: "Hello!"}));
+router.get('/', (req, res) => res.send("Hello!"));
+router.get('/notes', (req, res) => res.json({notes: "List of notes show up here"}));
+router.get('/notes/:id', (req, res) => res.json({id: `${req.params.id}`}));
+
+
+
 app.use('/api', router);
 app.listen(port, () => console.log(`Listening on port ${port}`));
